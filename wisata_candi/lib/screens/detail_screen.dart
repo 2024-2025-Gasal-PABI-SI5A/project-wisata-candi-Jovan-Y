9e98920aebf1b2e88490c2cb42645a3fb0a398b0
jovan <yometmank@gmail.com>
2024-11-09 10:52:49 +0700

    commit
    asdasd

---
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:candi/models/candi.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget{
+  final Candi candi;
+  const DetailScreen({super.key, this.candi});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+  
+}
